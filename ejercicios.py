7ab89f4b17802ed977b9c161d7003cf25f6476fc
Dynamight <saraialejandravidaurre@gmail.com>
2025-06-09 14:14:25 +0000

    edad_str= input(Bienvenido al cine,¿cual es tu edad?: )

---
+++ ejercicios.py
@@ -0,0 +1,10 @@
+edad_str= input(Bienvenido al cine,¿cual es tu edad?: )
+edad = int(edad_str)
+
+if edad<0:
+  print(edad no valida. Por favor, ingresa un numero positivo.)
+elif edad>=18:
+  print(¡puedes ver peliculas clasificadas R!)
+elif edad >=13:
+else: 
+  print (¡puedes ver la pelicula clasificada PG!)
