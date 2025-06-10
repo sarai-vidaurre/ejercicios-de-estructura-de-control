7e986370a93c99dd48c0c8300f4d8be30877d21e
Dynamight <saraialejandravidaurre@gmail.com>
2025-06-10 12:54:41 +0000

    corrección

--- ejercicios.py
+++ ejercicios.py
@@ -1,10 +1,11 @@
-edad_str= input(Bienvenido al cine,¿cual es tu edad?: )
+edad_str= input("Bienvenido al cine,¿cual es tu edad?:" )
 edad = int(edad_str)
 
 if edad<0:
-  print(edad no valida. Por favor, ingresa un numero positivo.)
+  print("edad no valida. Por favor, ingresa un numero positivo.")
 elif edad>=18:
-  print(¡puedes ver peliculas clasificadas R!)
+  print("¡puedes ver peliculas clasificadas R!")
 elif edad >=13:
+  print ("¡pudes ver peliculas clasificadas PG-13!")
 else: 
-  print (¡puedes ver la pelicula clasificada PG!)
+  print ("¡puedes ver la pelicula clasificada PG!")
