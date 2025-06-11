f43db4b729c431f885f98f71f974ef6ad7a73669
Dynamight <saraialejandravidaurre@gmail.com>
2025-06-11 12:47:53 +0000

    comidas favoritas

---
+++ clase04_listas.py
@@ -0,0 +1,5 @@
+comidas_favoritas = ["pizza", "hamburguesa", "pastas", "ramen","katsudon"]
+print (f"Lista original: {comidas_favoritas}")
+print(f"La primera comida favorita es: {comidas_favoritas[0]}")
+print(f"La segunda comida favorita es: {comidas_favoritas[1]}")
+print(f"La tercera comida favorita es: {comidas_favoritas[2]}")
