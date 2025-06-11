dcede5dac026d335f0669991b21de03756882f13
Dynamight <saraialejandravidaurre@gmail.com>
2025-06-11 13:17:17 +0000

    equipo 404 NOT FOUND

---
+++ lista_grupo.py
@@ -0,0 +1,4 @@
+print("¡Bienvenidos al equipo 404 NOT FOUND!")
+integrantes = ["Sarai Vidaurre", "Fabrizzio Lora", "Flavia Gutierrez", "Lizbeth", "Yoel Escalante","Rodrigo Miranda"]
+for nombre in integrantes:
+ print(f"¡Bienvenido al equipo, {nombre}!")
