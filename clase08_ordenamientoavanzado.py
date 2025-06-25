6b9a506b1cf29305ffba1a8f67250d88ac1cbbff
Dynamight <saraialejandravidaurre@gmail.com>
2025-06-25 12:28:00 +0000

    ordenamiento avanzado

--- clase08_ordenamientoavanzado.py
+++ clase08_ordenamientoavanzado.py
@@ -38,3 +38,4 @@ def merge(izquierda, derecha):
 mi_lista = [38, 27, 43, 3, 9, 82, 10]
 ordenada = merge_sort(mi_lista)
 print("Lista ordenada:", ordenada)
+print ("fin del programa Sarai Vidaurre")
